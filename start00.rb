8f5ea1838953921a6d5c640224d95b989d20a273
mbig <mbijeff@gmail.com>
2014-12-24 22:07:12 -0500

    added if statement

--- start00.rb
+++ start00.rb
@@ -9,3 +9,7 @@ ageTen = age + 10
 
 print(firstName.chomp + " " + lastName.chomp + " is ", age , " years old.\n")
 print("In 10 years you will be ", ageTen, " years old.\n")
+if (age > 60)
+puts("You are a senior citizen")
+end
+
