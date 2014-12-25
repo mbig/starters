b8f78433e2a968d533d6913811cbb91970982461
mbig <mbijeff@gmail.com>
2014-12-24 22:24:12 -0500

    ruby rangers test

---
+++ start03.rb
@@ -0,0 +1,13 @@
+#Ranges
+
+letters = 'a'..'z'
+puts(letters)
+puts("Does this range include the letter h?")
+answer = letters.include?('h')
+puts(answer)
+
+
+puts("Enter a letter to find out if it belongs to this range")
+answer = gets
+result = letters.include?(answer)
+puts(result)
