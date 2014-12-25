b3b2dcaa6a78ce46a18fc033f9de2cbe05ef0688
mbig <mbijeff@gmail.com>
2014-12-24 23:04:29 -0500

    ruby arrays

---
+++ start04.rb
@@ -0,0 +1,11 @@
+#Arrays
+
+id = [1,2,3,4,5]
+names = ["David", "Cynthia", "Raymond", "Bryan", "Chris"]
+subject = ["Biology", "Chemistry", "Math", "Physics", "Socilogy"]
+grades = [60, 80, 78, 90, 95]
+
+
+print("Student with id ", id[0],  " is ", names[0],  " scored ", grades[0],  " in ", subject[0], " \n")
+print("Student with id ", id[1],  " is ", names[1],  " scored ", grades[1],  " in ", subject[1], " \n")
+print("Student with id ", id[4],  " is ", names[4],  " scored ", grades[4],  " in ", subject[4], " \n")
