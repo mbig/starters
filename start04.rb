bbc2a64b439e465b2a9d98082d647cd94b0f7dfb
mbig <mbijeff@gmail.com>
2014-12-25 20:44:03 -0500

    added if clause

--- start04.rb
+++ start04.rb
@@ -9,3 +9,11 @@ grades = [60, 80, 78, 90, 95]
 print("Student with id ", id[0],  " is ", names[0],  " scored ", grades[0],  " in ", subject[0], " \n")
 print("Student with id ", id[1],  " is ", names[1],  " scored ", grades[1],  " in ", subject[1], " \n")
 print("Student with id ", id[4],  " is ", names[4],  " scored ", grades[4],  " in ", subject[4], " \n")
+
+if grades[3] >= 90
+ print(names[3] +  ", student with id: ", id[3], " got an A in ", subject[3], "\n")
+# puts(names[3], + " Student with id:",  + id[3],  + " got an A in ", subject[3]  )
+end
+
+
+
