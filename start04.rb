064f959327619445803234ff84612fdd40148fa1
mbig <mbijeff@gmail.com>
2014-12-25 21:54:27 -0500

    Added finding the Max and Min Value of a random Array

--- start04.rb
+++ start04.rb
@@ -12,8 +12,43 @@ print("Student with id ", id[4],  " is ", names[4],  " scored ", grades[4],  " i
 
 if grades[3] >= 90
  print(names[3] +  ", student with id: ", id[3], " got an A in ", subject[3], "\n")
-# puts(names[3], + " Student with id:",  + id[3],  + " got an A in ", subject[3]  )
+end
+
+puts
+puts("Find the Max and Min Value of a random Array")
+
+nums = []
+
+i = 0
+while i < 10
+# This will create random numbers strictly from 1 - 100
+   nums[i] = rand(101) 
+   i += 1
+end
+
+puts(nums)
+puts
+
+max = nums[0]
+
+for i in 1..9
+    if nums[i] > max
+        max = nums[i]
+    end
 end
 
 
+puts("Maximum value is: " +  max.to_s)
+
+puts("Finding the minimum value")
+
+min = nums[0]
+
+for i in 1..9
+      if nums[i] < min
+       min = nums[i]
+      end
+end
+
+puts("Minimum value is: " + min.to_s)
 
