6c607e41b795c6bc22a83946836ae628073073d8
mbig <mbijeff@gmail.com>
2014-12-25 20:58:38 -0500

    added converting a range to an array and print out some entries

--- start03.rb
+++ start03.rb
@@ -25,3 +25,15 @@ print("Enter a number to get if in range: ")
 num = gets
 result =  digits === Integer(num) 
 puts(result)
+
+puts("Convert a sequence to an Array")
+
+nums = digits.to_a
+
+print("First number in array: ", nums[0], "\n")
+print("Second entry in array: ", nums[1], "\n")
+print("Third entry in array: ", nums[2], "\n")
+print("Firth entry in array: ", nums[4], "\n")
+
+
+
