b560c03cf509aa48dbe100bcc4e5e4f7b962173f
mbig <mbijeff@gmail.com>
2014-12-28 16:12:54 -0500

    added until loop and converting number to string

--- start09.rb
+++ start09.rb
@@ -9,4 +9,15 @@ while number <= 10
      number += 1
 end
 
-puts(sum)
+puts("The sum of the numbers is: " + sum.to_s)
+
+
+sum = 0
+number = 1
+
+until number > 10
+   sum += number
+   number += 1
+end
+
+puts("The sum using an until loop is: " + sum.to_s)
