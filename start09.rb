1c3d3de2b8823981e5a49a091f56228f165b988e
mbig <mbijeff@gmail.com>
2014-12-28 16:48:36 -0500

    added 3 types of iterators

--- start09.rb
+++ start09.rb
@@ -21,3 +21,29 @@ until number > 10
 end
 
 puts("The sum using an until loop is: " + sum.to_s)
+
+
+#iterator
+#Times
+
+5.times do
+   print "Hello, World\n"
+end
+
+##Sum
+
+sum = 0
+1.upto(10) do |x|
+  sum += x
+end
+
+puts(sum)
+
+#step
+
+sum = 0
+1.step(10,2) {|x| sum += x }
+puts(sum)
+
+sum = 0
+2.step(100,2) {|x| print x, " "}
