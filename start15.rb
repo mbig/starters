79c38f2e7d57fc6061fbc0990176ee5cdf6e9997
mbig <mbijeff@gmail.com>
2014-12-28 18:44:19 -0500

    Exercises

---
+++ start15.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+
+
+def tipCalc(amount)
+   return amount * 0.15
+end
+
+print("Enter the amount of the bill: ")
+bill = Float(gets)
+tip = tipCalc(bill)
+total = bill + tip
+puts("Your total is " + total.to_s)
---
+++ start16.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/ruby
+
+def add(n1, n2) 
+   return n1 + n2
+end
+
+def subtract(n1, n2)
+   return n1 - n2
+end
+
+def mult(n1, n2)
+   return n1 * n2
+end
+
+def div(n1, n2) 
+   return n1 / n2
+end
+
+def calc(num1, num2, op)
+if op == "+"
+   puts(add(num1, num2))
+elsif op == "-"
+   puts(subtract(num1, num2))
+elsif op == "*"
+   puts(mult(num1, num2))
+elsif op == "/"
+   puts(div(num1, num2))
+else
+   puts("Bad operator")
+end
+
+end
+
+
+
+
+print("Enter first number: ")
+num1 = Float(gets)
+print("Enter second number: ")
+num2 = Float(gets)
+print("Enter operator: ")
+op = gets
+op = op.chomp
+
+print(calc(num1, num2, op))
+
+exit
+
+
+if op == "+"
+   puts(add(num1, num2))
+elsif op == "-"
+   puts(subtract(num1, num2))
+elsif op == "*"
+   puts(mult(num1, num2))
+elsif op == "/"
+   puts(div(num1, num2))
+else
+   puts("Bad operator")
+end
