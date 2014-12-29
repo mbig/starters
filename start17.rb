d020024f2224fe4c7c9a1b0c67577a52c87d0599
mbig <mbijeff@gmail.com>
2014-12-28 19:03:46 -0500

    classes and instance objects

---
+++ start17.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+
+
+class Name
+   def initialize(first, middle, last)
+      @first = first
+      @middle = middle
+      @last = last
+   end
+
+   def to_s
+      print(@last + ", " + @first + " " + @middle)
+   end
+end
+
+aName = Name.new("Jane","Elizabeth","Brown")
+#print aName.inspect
+print aName.to_s
+#print aName
+
+
+
+
