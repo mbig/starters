adebc648ad67067d8c0c669fa9afe862d0ad7d5e
mbig <mbijeff@gmail.com>
2014-12-28 19:04:59 -0500

    classes and instance objects updated

--- start17.rb
+++ start17.rb
@@ -1,23 +1,63 @@
 #!/usr/bin/ruby
 
-
 class Name
+
    def initialize(first, middle, last)
+
       @first = first
+
       @middle = middle
+
       @last = last
+
    end
+
+
+
+   def first
+
+      @first
+
+   end
+
+
+
+   def middle
+
+      @middle
+
+   end
+
+
+
+   def last
+
+      @last
+
+   end
+
+
 
    def to_s
+
       print(@last + ", " + @first + " " + @middle)
+
    end
+
 end
 
+
+
 aName = Name.new("Jane","Elizabeth","Brown")
-#print aName.inspect
+
 print aName.to_s
-#print aName
 
+print "\n"
 
+print "first name: " + aName.first
 
+print "\n"
 
+print "middle name: " + aName.middle
+
+print "\n"
